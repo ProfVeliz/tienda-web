db62a98697fba171cb046110fcbb03c54cd43e94
Cristian Veliz <veliz.cristian.22@gmail.com>
2026-04-21 20:11:57 -0600

    "Usted esta enviando un mensaje"

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 from flask import Flask
+import os
+from dotenv import load_dotenv
+
+# Cargar variables del archivo .env
+load_dotenv()
 
 app = Flask(__name__)
+
+# Obtener variables de entorno
+remitente = os.getenv("EMAIL_USER")
+clave = os.getenv("EMAIL_PASS")
 
 @app.route("/")
 def index():
     return "Sistema funcionando correctamente"
 
 if __name__ == "__main__":
-    app.run()
+    print("EMAIL_USER:", remitente)
+    print("EMAIL_PASS:", clave)
+    app.run(debug=True)
